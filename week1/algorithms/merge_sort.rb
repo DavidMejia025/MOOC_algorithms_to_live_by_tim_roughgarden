e0d0fe1769f29a8ed3a375bd39258fa3255637a7
David Mejia <davidndy@gmail.com>
2018-07-25 18:53:02 -0500

    Merge sort algorithm in complete

--- week1/algorithms/merge_sort.rb
+++ week1/algorithms/merge_sort.rb
@@ -1,4 +1,4 @@
-require "pry"
+require "pry-byebug"
 def merge_sort(x)
   middle = x.length/2
   #binding.pry
@@ -8,48 +8,23 @@ def merge_sort(x)
     x.each_with_index do|item, index| 
       index<middle ? x_1 << x[index] : x_2 << x[index] 
     end
-    #puts "#{merge_sort(x_2)}"
     if middle > 0
-      p x_1
-     result = sort(merge_sort(x_1), merge_sort(x_2))
-     p "The result is #{result}"
+     result = merge(merge_sort(x_1), merge_sort(x_2))
      result
     else
       x
     end
 end
 
-def sort2(x_2,y_2)
+def merge(x_2,y_2)
+  puts "The input is #{x_2} #{y_2}"
+
   y = []
       i = 0
       j = 0
       k = 0
 
-      #x_2.length*2.times do |k|
-              
-      #         p "#{x_2[i]} #{y_2[j]}"
-      #         binding.pry
-      #   if x_2[i] < y_2[j]
-      #     y[k] = x_2[i]
-      #     i+=1
-      #   else
-      #     y[k] = y_2[j]
-      #     j+=1
-      #   end
-      # end
-      [y_2,x_2] 
-      [y_2,x_2].flatten 
-end
-
-def sort(x_2,y_2)
-  puts "The input is #{x_2}"
-  y = []
-      i = 0
-      j = 0
-      k = 0
-
-      x_2.length*2.times do |k|
-              
+      (x_2.length*2).times do |k|
               #p "#{x_2[i]} #{y_2[j]}"     
         if x_2[i] && y_2[j]
           if x_2[i] < y_2[j]
@@ -61,9 +36,11 @@ def sort(x_2,y_2)
           end
         elsif x_2[i]
             y[k] = x_2[i]
+            i += 1
 
         else
             y[k] = y_2[j]
+            j += 1
         end
         y
       end 
@@ -71,5 +48,5 @@ def sort(x_2,y_2)
       y
 end
 
-x = [4,3,5,2,8,1,7,6] 
+x = [4,10,3,16,2,11,1,9,12,13,6,14,5,7,2,8] 
  p merge_sort(x)
